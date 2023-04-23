8cf8fb83d35652af5946dce5cbd5eee72fa51db6
mod728 <vinnybeach712@yahoo.co.jp>
2023-04-23 10:49:54 +0900

    Code

---
+++ README.md
@@ -0,0 +1,12 @@
+# バックエンドサーバー（Node.js & Express）
+「はじめてつくるバックエンドサーバー基礎編（Node.js & Express）」
+
+Expressを使った開発（第2-3章）と使わない開発（第4章）のソースコードです。
+
+（2023年4月版）
+
+---
+
+本書の続編「はじめてつくるバックエンドサーバー発展編（Node.js & Express & MongoDB）」
+
+https://www.amazon.co.jp/dp/B09X56PSCQ
---
+++ node-vanilla/index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+http.createServer(function (req, res) { 
+        res.setHeader("Content-Type", "text/plain;charset=utf-8");
+        res.setHeader("Content-Type", "text/html"); 
+        if(req.url === "/" && req.method === "GET") {
+            res.write("こんにちは");
+            res.end();
+        } else if(req.url === "/about" && req.method === "GET") { 
+            res.write("ここはAboutページです。"); 
+            res.end();
+        } else if(req.url === "/hobby" && req.method === "GET") {
+            res.write('<form action="/outdoor" method="POST"><input type="text" name="sports"><button type="submit">Submit</button></form>');
+            res.end(); 
+        } else if(req.url === "/outdoor" && req.method === "POST") { 
+            console.log("reqの中身:", req);
+        } 
+    }).listen(4005, function() { 
+        console.log("Listening on localhost port 4000"); 
+}); 
